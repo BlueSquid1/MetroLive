478c6d1efde78e1d216dabf274e00b2dafc0e9e2
Clinton Page <clintonpage@Clintons-MacBook-Air.local>
2017-02-21 13:42:33 +1030

    added GTFS

---
+++ MetroLive/MetroLive/BusStop.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using GTFS;
+using GTFS.Exceptions;
+using GTFS.IO;
+namespace MetroLive
+{
+	public class BusStopMgr
+	{
+		private GTFSReader<GTFSFeed> Reader;
+
+		//constructor
+		public BusStopMgr(int busStopId)
+		{
+			Reader = new GTFSReader<GTFSFeed>();
+			//GTFSDirectorySource x = new GTFSDirectorySource();
+		}
+	}
+}
---
+++ MetroLive/MetroLive/GTFSLoader.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace MetroLive
+{
+	public class GTFSLoader
+	{
+		public GTFSLoader()
+		{
+		}
+	}
+}
