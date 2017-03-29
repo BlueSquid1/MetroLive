586f7410fc5b3190ccdefedf1323e859673b87b2
Clinton Page <clinton.page123@gmail.com>
2017-03-30 00:30:52 +1030

    fixed UI display issues

--- MetroLive/MetroLive/Pages/StopDetailPage.xaml.cs
+++ MetroLive/MetroLive/Pages/StopDetailPage.xaml.cs
@@ -15,9 +15,11 @@ namespace MetroLive.Pages
 {
     public class BusViewModel
     {
-        public string BusName { get; set; }
+        public string LineRef { get; set; }
         public string ExpectArrival { get; set; }
-        public string EarlyDiff { get; set; }
+        public string ExpectedUncertainty { get; set; }
+        public string TimeDiff { get; set; }
+        public string TimeTillArrival { get; set; }
     }
 
     public partial class StopDetailPage : ContentPage
@@ -30,7 +32,6 @@ namespace MetroLive.Pages
         {
             InitializeComponent();
             this.metroLive = mMetroLive;
-
             this.Appearing += StopDetailsView_Appearing;
 
             stopMgr = mMetroLive.GetBusStopDetails(busReference);
@@ -39,13 +40,6 @@ namespace MetroLive.Pages
         //triggered when page is about to be displayed
         private async void StopDetailsView_Appearing(object sender, EventArgs e)
         {
-            /*
-            await stopMgr.FetchscheduledDataAsync(new DateTimeOffset(DateTime.Now, TimeSpan.FromMinutes(120)));
-            UpdateDisplay();
-            await stopMgr.FetchLRealTimeDataAsync(new DateTimeOffset(DateTime.Now + metroLive.Settings.SIRIStart, metroLive.Settings.SIRIPreviewInterval));
-            UpdateDisplay();
-            */
-
             //get realtime info
             BusStopDetails stopDetails = await stopMgr.GetRealTimeDataAsync(new DateTimeOffset(DateTime.Now.Ticks, TimeSpan.FromMinutes(60)));
             UpdateDisplay(stopDetails);
@@ -58,8 +52,25 @@ namespace MetroLive.Pages
             //populate the collection
             foreach ( VehicleJourney vehicle in stopDetails.IncomingVehicles )
             {
-                TimeSpan? earilyDiff = vehicle.AimedArrival - vehicle.EarliestEstimatedArrival;
-                busCollection.Add(new BusViewModel { BusName = vehicle.LineRef, ExpectArrival = vehicle.EarliestEstimatedArrival.ToString(), EarlyDiff = earilyDiff.ToString() });
+                string lineRef = vehicle?.LineRef;
+                TimeSpan? realTimeUncertainty = vehicle?.LatestEstimatedArrival - vehicle?.EarliestEstimatedArrival;
+                string uncertainty = "+-" + Math.Abs(realTimeUncertainty.Value.Minutes).ToString();
+                TimeSpan? realTimeDiff = vehicle.AimedArrival - vehicle.EarliestEstimatedArrival;
+                string timeDiff = realTimeDiff.Value.Minutes.ToString() + " mins";
+                DateTime? estimateAvg = vehicle?.EarliestEstimatedArrival;
+                estimateAvg.Value.AddMinutes(realTimeUncertainty.Value.Minutes / 2);
+                string estimateAvgString = estimateAvg.Value.TimeOfDay.ToString();
+                TimeSpan? timeTillArrival = estimateAvg - DateTime.Now;
+                string timeTillArrivalStr = timeTillArrival.Value.TotalMinutes.ToString();
+
+                busCollection.Add(new BusViewModel
+                {
+                    ExpectedUncertainty = uncertainty,
+                    TimeDiff = timeDiff,
+                    LineRef = lineRef,
+                    ExpectArrival = estimateAvgString,
+                    TimeTillArrival = timeTillArrivalStr
+                });
             }
             listView.ItemsSource = busCollection;
         }
